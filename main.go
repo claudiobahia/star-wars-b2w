b72e755627f9d3f8fab3c51e0e6d15996d2c96ab
claudiobahia <claudio.bahia@al.infnet.edu.br>
2020-03-16 22:29:20 -0300

    MongoDB connection working fine

--- main.go
+++ main.go
@@ -1,31 +1,53 @@
 package main
 
+// consuming star wars api planets to mongo db, create rest api with those datas
+
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Planet struct {
-	Name    string
-	Climate string
-	Terrain string
+	ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name    string             `json:"name,omitempty" bson:"name,omitempty"`
+	Climate string             `json:"climate,omitempty" bson:"climate,omitempty"`
+	Terrain string             `json:"terrain,omitempty" bson:"terrain,omitempty"`
 }
 
-func getPlanet(pNum int) {
+func getAllPlanets() {
 	//total of 61 planets
-	response, err := http.Get("https://swapi.co/api/planets/" + string(pNum))
-	if err != nil {
-		panic(err)
-	} else {
-		var planet Planet
-		data, _ := ioutil.ReadAll(response.Body)
-		json.Unmarshal([]byte(string(data)), &planet)
-		fmt.Println(planet)
-	}
+	response, _ := http.Get("https://swapi.co/api/planets/")
+	var planet Planet
+	data, _ := ioutil.ReadAll(response.Body)
+	str := json.Unmarshal([]byte(string(data)), &planet)
+}
+
+var client *mongo.Client
+
+func CreatePlanetEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	var planet Planet
+	json.NewDecoder(request.Body).Decode(&planet)
+	collection := client.Database("starwars").Collection("planets")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	result, _ := collection.InsertOne(ctx, planet)
+	json.NewEncoder(response).Encode(result)
 }
 
 func main() {
-
+	fmt.Println("Starting")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	router := mux.NewRouter()
+	router.HandleFunc("/planet", CreatePlanetEndpoint).Methods("POST")
+	http.ListenAndServe(":12345", router)
 }
