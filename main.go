d8f5d37a55e8057ceea1bb2c561716495dd55e9a
claudiobahia <claudio.bahia@al.infnet.edu.br>
2020-03-17 15:14:14 -0300

    Needs delete get all and get name/id.

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var client *mongo.Client
@@ -24,32 +26,66 @@ type Planet struct {
 	Films   []string           `json:"films,omitempty" bson:"films,omitempty"`
 }
 
-func getPlanet(pNum string) {
-	//total of 61 planets
+// ------------------------------------------------ test start
+type PlanetsAPI struct {
+	Count    int
+	Next     string
+	previous string
+	Results  []Planet
+}
+
+func getPlanetsFromAPI() {
+	response, _ := http.Get("https://swapi.co/api/planets")
+	var planets PlanetsAPI
+	data, _ := ioutil.ReadAll(response.Body)
+	json.Unmarshal(data, &planets)
+	fmt.Println(planets.Results)
+}
+
+// ------------------------------------------------ tests end
+
+func getPlanetFromAPIbyID(pNum string) {
 	response, _ := http.Get("https://swapi.co/api/planets/" + pNum)
 	var planet Planet
 	data, _ := ioutil.ReadAll(response.Body)
-	json.Unmarshal([]byte(string(data)), &planet)
-	fmt.Println(planet)
+	json.Unmarshal(data, &planet)
+	//
 }
 
-func CreatePlanetEndpoint(response http.ResponseWriter, request *http.Request) {
+func getPlanetFromAPIbyName(name string) {
+	response, _ := http.Get("https://swapi.co/api/planets:search=" + name)
+	var planet Planet
+	data, _ := ioutil.ReadAll(response.Body)
+	json.Unmarshal(data, &planet)
+	//
+}
+
+func POSTplanet(response http.ResponseWriter, request *http.Request) {
 	response.Header().Add("content-type", "application/json")
 	var planet Planet
 	json.NewDecoder(request.Body).Decode(&planet)
+
+	/////// TODO todo Todo here will be the verification if it returns OK
+
 	collection := client.Database("starwars").Collection("planets")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	result, _ := collection.InsertOne(ctx, planet)
 	json.NewEncoder(response).Encode(result)
 }
 
+func GETallPlanetsFromMongoDB() {
+
+}
+
+func DELETEplanetFromMongoDB() {
+
+}
+
 func main() {
-	// fmt.Println("Starting")
-	// ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	// client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	// router := mux.NewRouter()
-	// router.HandleFunc("/planet", CreatePlanetEndpoint).Methods("POST")
-	// http.ListenAndServe(":12345", router)
-	x := "2"
-	getPlanet(x)
+	fmt.Println("Starting")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	router := mux.NewRouter()
+	router.HandleFunc("/planet", POSTplanet).Methods("POST")
+	http.ListenAndServe(":12345", router)
 }
